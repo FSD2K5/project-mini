36f3f85aeb1f5c9cd606c0c3d5447cd343e9fd8b
FSD2K5 <horizon3470@gmail.com>
2023-01-23 06:09:34 +0700

    add project

---
+++ slider/assets/js/index.js
@@ -0,0 +1,92 @@
+const data = [
+    {
+        id: 1,
+        link: "./assets/images/bg1.jpg",
+    },
+    {
+        id: 2,
+        link: "./assets/images/bg2.jpg",
+    },
+    {
+        id: 3,
+        link: "./assets/images/bg3.jpg",
+    },
+    {
+        id: 4,
+        link: "./assets/images/bg4.jpeg",
+    },
+    {
+        id: 5,
+        link: "./assets/images/bg5.jpg",
+    },
+    {
+        id: 6,
+        link: "./assets/images/bg6.png",
+    },
+    {
+        id: 7,
+        link: "./assets/images/bg7.jpg",
+    },
+    {
+        id: 8,
+        link: "./assets/images/bg8.png",
+    },
+];
+
+const slider = document.querySelector(".list-img");
+const nextButton = document.querySelector("#next button");
+const previousButton = document.querySelector("#previous button");
+const listRadioButton = document.querySelector(".list-radio-button");
+let position = 0;
+let lengthData = data.length;
+let distance = 100 / lengthData;
+
+const listImg = function (list) {
+    const data = list.map((item) => {
+        return `
+            <li><img src="${item.link}"/></li>
+        `;
+    });
+    return data.join();
+};
+
+const handleChoose = function (listNode) {
+    listNode.forEach((item) => {
+        item.addEventListener("click", (e) => {
+            let pos = item.getAttribute("id");
+            console.log(pos);
+            slider.style.transform = `translateX(-${distance * pos}%)`;
+        });
+    });
+};
+
+const renderRadioButton = function (len) {
+    let id = 0;
+    for (let i = 0; i < len; i++) {
+        const li = document.createElement("li");
+        li.setAttribute("id", id);
+        listRadioButton.appendChild(li);
+        id = id + 1;
+    }
+    handleChoose(document.querySelectorAll(".list-radio-button li"));
+};
+renderRadioButton(lengthData);
+
+slider.innerHTML = listImg(data);
+//12.5
+nextButton.addEventListener("click", (e) => {
+    if (position > -(100 - distance)) {
+        position = position - distance;
+    } else {
+        position = 0;
+    }
+    slider.style.transform = `translateX(${position}%)`;
+});
+previousButton.addEventListener("click", (e) => {
+    if (position < 0) {
+        position = position + distance;
+    } else {
+        position = -(100 - distance);
+    }
+    slider.style.transform = `translateX(${position}%)`;
+});
